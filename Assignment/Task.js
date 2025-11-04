89e110b17586f4310c85877a340756744be06cef
mohan <mohanjadhav1308@gmail.com>
2025-11-04 15:01:31 +0530

    practice Assignment

--- Assignment/Task.js
+++ Assignment/Task.js
@@ -4,13 +4,20 @@ let coustomer = document.getElementById('btn2')
 function fetchcustdata() {
     fetch('https://playground.mockoon.com/customers')
         .then(res => res.json())
-        .then(data =>
+        .then(data => {
+            const tbody = document.getElementById('coustomertable')
+            tbody.innerHTML = "";
             data.forEach(element => {
-                console.log(element.name)
-                console.log(element.email)
-                console.log(element.phone)
-
-            }),
+                const row = `
+               <tr>
+                    <td>${element.name}</td>
+                    <td>${element.email}</td>
+                    <td>${element.phone}</td>
+               </tr>
+               `
+                tbody.innerHTML = tbody.innerHTML + row;
+            })
+        }
         ).catch((error) => console.log("error in fetching customer data", error));
 }
 coustomer.addEventListener('click', fetchcustdata)
@@ -19,9 +26,23 @@ coustomer.addEventListener('click', fetchcustdata)
 function fetchimgdata() {
     fetch('https://playground.mockoon.com/photos')
         .then(res => res.json())
-        .then(data => data.forEach(element => {
-            console.log(element.url)
-        }))
+        .then(data => {
+            const tbody = document.getElementById('Imagetable')
+            tbody.innerHTML = "";
+            data.forEach(element => {
+                const row = `
+               <tr>
+                    <td><img src="${element.url}"></td>
+                    <td>${element.caption}</td>
+                    <td>${element.likes}</td>
+               </tr>
+               `
+                tbody.innerHTML = tbody.innerHTML + row;
+
+
+            })
+        }
+        )
         .catch((error) => console.log("error in fetching img", error))
 
 }
